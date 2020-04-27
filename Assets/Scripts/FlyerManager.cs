0001f26f0c241074f90116de4ac08aadf9d172a3
Jonathan Moallem <jdmoallem@gmail.com>
2020-04-27 12:54:06 +1000

    Made flyers display event titles and descriptions

---
+++ Assets/Scripts/FlyerManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class FlyerManager : MonoBehaviour
+{
+    [SerializeField] private Text titleText;
+    [SerializeField] private Text descriptionText;
+
+    public void SetTitle(string title)
+    {
+        titleText.text = title;
+    }
+
+    public void SetDescription(string description)
+    {
+        descriptionText.text = description;
+    }
+}
--- Assets/Scripts/NoticeboardController.cs
+++ Assets/Scripts/NoticeboardController.cs
@@ -1,17 +1,39 @@
 ﻿using UnityEngine;
-using UnityEngine.UI;
 
 public class NoticeboardController : MonoBehaviour
 {
-    [SerializeField] private Text textList;
+    [SerializeField] private GameObject[] flyerList;
+
+    private void Start()
+    {
+        Display();
+    }
 
     public void Display()
     {
-        textList.text = string.Join("\n", GetDummyEvents());
+        var events = GetDummyEvents();
+        for (var i = 0; i < flyerList.Length; i++)
+        {
+            
+            if (i < events.Length)
+            {
+                var fm = flyerList[i].GetComponent<FlyerManager>();
+                fm.SetTitle(events[i].ScenarioTitle);
+                fm.SetDescription(events[i].ScenarioText);
+            }
+            else
+                flyerList[i].SetActive(false);
+        }
     }
 
-    private static string[] GetDummyEvents()
+    private static Event[] GetDummyEvents()
     {
-        return new [] {"Real Fake Event 1", "Real Fake Event 2"};
+        var event1 = ScriptableObject.CreateInstance<Event>();
+        event1.ScenarioTitle = "Real Fake Event 1";
+        event1.ScenarioText = "This event is brought to you by Scriptable Object 1";
+        var event2 = ScriptableObject.CreateInstance<Event>();
+        event2.ScenarioTitle = "Real Fake Event 2";
+        event2.ScenarioText = "This event is brought to you by Scriptable Object 2";
+        return new [] {event1, event2};
     }
 }
