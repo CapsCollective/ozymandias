f3fe897c2e440b035b55d5e668b680260aec99e8
Jonathan Moallem <jdmoallem@gmail.com>
2020-04-28 13:37:05 +1000

    Commented, formatted and abbreviated noticeboard and flyer manager classes

--- Assets/Scripts/FlyerManager.cs
+++ Assets/Scripts/FlyerManager.cs
@@ -6,17 +6,9 @@ public class FlyerManager : MonoBehaviour
     [SerializeField] private Text titleText;
     [SerializeField] private Text descriptionText;
 
-    private Event @event;
-
     public void SetEvent(Event e)
     {
-        @event = e;
-        titleText.text = @event.ScenarioTitle;
-        descriptionText.text = @event.ScenarioText;
-    }
-    
-    public Event GetEvent()
-    {
-        return @event;
+        titleText.text = e.ScenarioTitle;
+        descriptionText.text = e.ScenarioText;
     }
 }
--- Assets/Scripts/NoticeboardController.cs
+++ Assets/Scripts/NoticeboardController.cs
@@ -4,114 +4,46 @@ using UnityEngine;
 
 public class NoticeboardController : MonoBehaviour
 {
+    // Fields
     [SerializeField] private GameObject[] flyerList;
-
     private Dictionary<string, GameObject> flyerMappings = new Dictionary<string, GameObject>();
 
-    private void Update()
+    public void UpdateDisplay()
     {
-        if (Input.GetKey("q"))
-        {
-            Update(GetDummyEvents1());
-        }
-        if (Input.GetKey("w"))
-        {
-            Update(GetDummyEvents2());
-        }
-        if (Input.GetKey("e"))
-        {
-            Update(GetDummyEvents3());
-        }
-        if (Input.GetKey("r"))
-        {
-            Update(GetDummyEvents4());
-        }
-        if (Input.GetKey("t"))
-        {
-            Update(GetDummyEvents5());
-        }
-    }
+        // Fetch the currently active events
+        var events = GetEvents();
 
-    public void Update(Event[] eventlist)
-    {
-        var events = eventlist;
-        var newMap = new Dictionary<string, GameObject>();
+        // Create a new mapping for previously posted events
+        var newMappings = events.Where(ev => flyerMappings.ContainsKey(ev.ScenarioTitle))
+            .ToDictionary(ev => ev.ScenarioTitle, ev => flyerMappings[ev.ScenarioTitle]);
         
-        foreach (var ev in events)
-        {
-            if (flyerMappings.ContainsKey(ev.ScenarioTitle))
-            {
-                print("found a copy");
-                newMap.Add(ev.ScenarioTitle, flyerMappings[ev.ScenarioTitle]);
-            }
-        }
-        flyerMappings = newMap;
+        // Remove events that have already been mapped
+        events = events.Where(e => !newMappings.Keys.Contains(e.ScenarioTitle)).ToArray();
+        
+        // Create a shuffled array of unused flyers
         var unusedFlyers = flyerList.Where(f => !flyerMappings.Values.Contains(f)).ToArray()
             .OrderBy(x => new System.Random().Next(1, 8)).ToArray();
         
-        events = events.Where(e => !flyerMappings.Keys.Contains(e.ScenarioTitle)).ToArray();
-        for (var i = 0; i < availableFlyers.Length; i++)
+        // Assign the remaining events to the unused flyers, setting their states and recording mappings
+        for (var i = 0; i < unusedFlyers.Length; i++)
         {
             if (i < events.Length)
             {
-                availableFlyers[i].SetActive(true);
-                availableFlyers[i].GetComponent<FlyerManager>().SetEvent(events[i]);
-                flyerMappings.Add(events[i].ScenarioTitle, availableFlyers[i]);
+                unusedFlyers[i].SetActive(true);
+                unusedFlyers[i].GetComponent<FlyerManager>().SetEvent(events[i]);
+                newMappings.Add(events[i].ScenarioTitle, unusedFlyers[i]);
             }
             else
-                availableFlyers[i].SetActive(false);
+                unusedFlyers[i].SetActive(false);
         }
+        
+        // Set the new flyer mappings
+        flyerMappings = newMappings;
     }
 
-    private static Event[] GetDummyEvents1()
+    private Event[] GetEvents()
     {
-        var event1 = ScriptableObject.CreateInstance<Event>();
-        event1.ScenarioTitle = "Real Fake Event 1";
-        event1.ScenarioText = "This event is brought to you by Scriptable Object 1";
-        var event2 = ScriptableObject.CreateInstance<Event>();
-        event2.ScenarioTitle = "Real Fake Event 2";
-        event2.ScenarioText = "This event is brought to you by Scriptable Object 2";
-        return new [] {event1, event2};
+        throw new System.NotImplementedException();
     }
-    
-    private static Event[] GetDummyEvents2()
-    {
-        var event2 = ScriptableObject.CreateInstance<Event>();
-        event2.ScenarioTitle = "Real Fake Event 2";
-        event2.ScenarioText = "This event is brought to you by Scriptable Object 2";
-        var event3 = ScriptableObject.CreateInstance<Event>();
-        event3.ScenarioTitle = "Real Fake Event 3";
-        event3.ScenarioText = "This event is brought to you by Scriptable Object 3";
-        return new [] {event2, event3};
-    }
-    
-    private static Event[] GetDummyEvents3()
-    {
-        var event1 = ScriptableObject.CreateInstance<Event>();
-        event1.ScenarioTitle = "Real Fake Event 1";
-        event1.ScenarioText = "This event is brought to you by Scriptable Object 1";
-        var event3 = ScriptableObject.CreateInstance<Event>();
-        event3.ScenarioTitle = "Real Fake Event 3";
-        event3.ScenarioText = "This event is brought to you by Scriptable Object 3";
-        var event4 = ScriptableObject.CreateInstance<Event>();
-        event4.ScenarioTitle = "Real Fake Event 4";
-        event4.ScenarioText = "This event is brought to you by Scriptable Object 4";
-        var event5 = ScriptableObject.CreateInstance<Event>();
-        event5.ScenarioTitle = "Real Fake Event 5";
-        event5.ScenarioText = "This event is brought to you by Scriptable Object 5";
-        return new [] {event4, event3, event1, event5};
-    }
-    
-    private static Event[] GetDummyEvents4()
-    {
-        var event4 = ScriptableObject.CreateInstance<Event>();
-        event4.ScenarioTitle = "Real Fake Event 4";
-        event4.ScenarioText = "This event is brought to you by Scriptable Object 4";
-        return new [] {event4};
-    }
-    
-    private static Event[] GetDummyEvents5()
-    {
-        return new Event[] {};
-    }
+
 }
