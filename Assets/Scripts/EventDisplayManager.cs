7e6b37a48db6dc2e06e1300872db144520625b2a
Edisuism <53289506+Edisuism@users.noreply.github.com>
2020-05-05 20:07:33 +1000

    dragtext

---
+++ Assets/Scripts/EventDisplayManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class EventDisplayManager : MonoBehaviour
+{
+    [SerializeField] private Text titleText;
+    [SerializeField] private Text descriptionText;
+
+    public void SetEvent(Event e)
+    {
+        titleText.text = e.ScenarioTitle;
+        descriptionText.text = e.ScenarioText;
+    }
+}
