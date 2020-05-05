82a5aeaef275d15798bf74be7cbe16b22db579ae
Swiggies <38583668+Swiggies@users.noreply.github.com>
2020-05-05 22:30:10 +1000

    Made Newspaper work with Events

---
+++ Assets/Scripts/ShowOnNewTurn.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowOnNewTurn : MonoBehaviour
+{
+    void Awake()
+    {
+        GameManager.OnNewTurn += () => gameObject.SetActive(true);
+        gameObject.SetActive(false);
+    }
+
+}
