095ef560c409d0ddf1b89699d9397be596c13753
Ben Kelly <benkellysoftware@gmail.com>
2020-05-19 10:59:17 +1000

    Replaced stat text with icons.

--- Assets/Scripts/UI/SideBar.cs
+++ Assets/Scripts/UI/SideBar.cs
@@ -6,19 +6,14 @@ using static GameManager;
 
 public class SideBar : UiUpdater
 {
-    public Text adventurers, satisfaction, effectiveness;//, threat, defense;
+    public Text adventurers, satisfaction, effectiveness, spending;//, threat, defense;
     // Update is called once per frame
     public override void UpdateUi()
     {
-        adventurers.text = "Adventurers: " + 
-                           Manager.AvailableAdventurers +
-                           " / " +
-                           Manager.Accommodation;
-        
-        satisfaction.text = "Satisfaction: " + Manager.Satisfaction + "%";
-
-        effectiveness.text = "Effectiveness: " + Manager.Effectiveness + "%";
-
+        adventurers.text = Manager.AvailableAdventurers + " / " + Manager.Accommodation;
+        satisfaction.text = Manager.Satisfaction + "%";
+        effectiveness.text = Manager.Effectiveness + "%";
+        spending.text = Manager.Spending + "%";
         //threat.text = "Threat: " + Manager.Threat;
         //defense.text = "Defense: " + Manager.Defense;
     }
