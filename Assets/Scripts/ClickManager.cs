3c5d53bc2e004d13e3e156f1ee750142c12644e4
Ben Kelly <benkellysoftware@gmail.com>
2020-05-21 00:17:05 +1000

    Created click manager for checking for quick clicks and assigned it to place

---
+++ Assets/Scripts/ClickManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ClickManager : MonoBehaviour
+{
+    [Range(0.1f,0.5f)]
+    public float clickSpeed = 0.2f;
+
+    public static Action OnLeftClick;
+    public static Action OnRightClick;
+    private float time0, time1;
+    
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) time0 = Time.time;
+        if (Input.GetMouseButtonUp(0) && Time.time - time0 < clickSpeed) OnLeftClick.Invoke();
+        
+        if (Input.GetMouseButtonDown(1)) time1 = Time.time;
+        if (Input.GetMouseButtonUp(1) && Time.time - time1 < clickSpeed) OnRightClick.Invoke();    
+    }
+}
--- Assets/Scripts/Menu/Place.cs
+++ Assets/Scripts/Menu/Place.cs
@@ -7,22 +7,21 @@ using static GameManager;
 
 public class Place : MonoBehaviour
 {
-    EventSystem eventSystem;
-    public Image image;
     public Map map;
     public Click selectedObject;
     public Hover hoverObject;
     public GameObject buildingInstantiated;
     private RaycastHit hit;
     private Camera cam;
-
+    
     // HIGHLIGHTING
     private Cell[] highlighted = new Cell[0];
 
     private void Awake()
     {
         cam = Camera.main;
-        eventSystem = EventSystem.current;
+        ClickManager.OnLeftClick += LeftClick;
+        ClickManager.OnRightClick += RightClick;
     }
 
     void Update()
@@ -30,73 +29,73 @@ public class Place : MonoBehaviour
         // Clear previous highlights
         map.Highlight(highlighted, Map.HighlightState.Inactive);
         highlighted = new Cell[0];
+        
+        if (selectedObject) Highlight();
+    }
 
-        if (selectedObject)
+    private void Highlight()
+    {
+        Ray ray = cam.ScreenPointToRay(new Vector3(Input.mousePosition.x, Input.mousePosition.y, cam.nearClipPlane));
+        Physics.Raycast(ray, out hit);
+        if (!buildingInstantiated)
+        {
+            if (hit.collider)
+            {
+                buildingInstantiated = Instantiate(selectedObject.building, hit.point, transform.rotation);
+            }
+        }
+        else
+        {
+            buildingInstantiated.transform.position = hit.point;
+        }
+
+        // Check if new cells need to be highlighted
+        Cell closest = map.GetCell(hit.point);
+        BuildingStructure building = selectedObject.building.GetComponent<BuildingStructure>();
+        Cell[] cells = map.GetCells(closest, building);
+
+        // Check if cells are valid
+        bool valid = map.IsValid(cells);
+
+        // Highlight cells
+        highlighted = cells;
+
+        Map.HighlightState state = valid ? Map.HighlightState.Valid : Map.HighlightState.Invalid;
+        map.Highlight(highlighted, state);
+    }
+    
+    private void LeftClick()
+    {
+        if (selectedObject && Manager.CurrentWealth >= selectedObject.building.GetComponent<BuildingStats>().baseCost)
         {
             Ray ray = cam.ScreenPointToRay(new Vector3(Input.mousePosition.x, Input.mousePosition.y, cam.nearClipPlane));
             Physics.Raycast(ray, out hit);
-            if (!buildingInstantiated)
+            if (hit.collider && !EventSystem.current.IsPointerOverGameObject())
             {
-                if (hit.collider)
-                {
-                    buildingInstantiated = Instantiate(selectedObject.building, hit.point, transform.rotation);
-                }
-            }
-            else
-            {
-                buildingInstantiated.transform.position = hit.point;
-            }
-
-            if (Input.GetMouseButtonDown(1))
-            {
-                buildingInstantiated.transform.Rotate(0,30,0);
-            }
-
-            // Check if new cells need to be highlighted
-            Cell closest = map.GetCell(hit.point);
-            BuildingStructure building = selectedObject.building.GetComponent<BuildingStructure>();
-            Cell[] cells = map.GetCells(closest, building);
-
-            // Check if cells are valid
-            bool valid = map.IsValid(cells);
-
-            // Highlight cells
-            highlighted = cells;
-
-            Map.HighlightState state = valid ? Map.HighlightState.Valid : Map.HighlightState.Invalid;
-            map.Highlight(highlighted, state);
-        }
-
-
-        if (Input.GetMouseButtonDown(0))
-        {
-            if (selectedObject && Manager.CurrentWealth >= selectedObject.building.GetComponent<BuildingStats>().baseCost)
-            {
-                Ray ray = Camera.main.ScreenPointToRay(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane));
-                Physics.Raycast(ray, out hit);
-                if (hit.collider && !EventSystem.current.IsPointerOverGameObject())
-                {
-                    Destroy(buildingInstantiated);
-
-                    map.CreateBuilding(selectedObject.building, hit.point);
-
-                    selectedObject = null;
-                }
-            }
-        }
-
-        if (Input.GetMouseButtonDown(1)) {
-            if (hoverObject && hoverObject.isHovered && !hoverObject.instantiatedHelper)
-            {
-                hoverObject.InfoBox();
-            }
-            else if (hoverObject && hoverObject.isHovered && hoverObject.instantiatedHelper)
-            {
-                Destroy(hoverObject.instantiatedHelper);
+                Destroy(buildingInstantiated);
+                map.CreateBuilding(selectedObject.building, hit.point);
+                selectedObject = null;
             }
         }
     }
-
+    
+    private void RightClick()
+    {
+        //TODO: Why is this here? Do this in hover instead
+        if (hoverObject && hoverObject.isHovered && !hoverObject.instantiatedHelper)
+        {
+            hoverObject.InfoBox();
+        }
+        else if (hoverObject && hoverObject.isHovered && hoverObject.instantiatedHelper)
+        {
+            Destroy(hoverObject.instantiatedHelper);
+        }
+        
+        if (!selectedObject) return;
+        //TODO: Replace with actual rotation
+        buildingInstantiated.transform.Rotate(0,30,0);
+    }
+    
     public void NewSelection()
     {
         Destroy(buildingInstantiated);
