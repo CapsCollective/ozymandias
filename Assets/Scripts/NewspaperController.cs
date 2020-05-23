67473ac57ad8ab7c89fb090bcd5e11ae158f82e2
Ben Kelly <benkellysoftware@gmail.com>
2020-05-23 23:04:42 +1000

    Added a ton of event images and made it so it would hide if no image available

--- Assets/Scripts/NewspaperController.cs
+++ Assets/Scripts/NewspaperController.cs
@@ -28,7 +28,8 @@ public class NewspaperController : MonoBehaviour
         if (choiceEvent.choices.Count > 0) continueButton.enabled = false;
         
         // Set the image for the main article and a newspaper title
-        if(!events[0].image) articleImage.sprite = events[0].image;
+        if(events[0].image) articleImage.sprite = events[0].image;
+        articleImage.gameObject.SetActive(events[0].image);
         newspaperTitle.text = GetNewspaperTitle();
 
         // Assign the remaining events to the corresponding spots
