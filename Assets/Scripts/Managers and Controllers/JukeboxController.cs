714c88db74fd43d8ed0b3e84dcc82bd5fd49decc
Jonathan Moallem <jdmoallem@gmail.com>
2020-05-30 21:25:57 +1000

    Created jukebox controller script to help manage game sound
    Former-commit-id: bb954d6dc58e312c9253faa52d444c4bee10bf7f

---
+++ Assets/Scripts/Managers and Controllers/JukeboxController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using NaughtyAttributes;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Managers_and_Controllers
+{
+    public class JukeboxController : MonoBehaviour
+    {
+        [SerializeField] private AudioSource ambiancePlayer;
+        [SerializeField] private AudioSource musicPlayer;
+        [SerializeField] private AudioClip[] tracks;
+        private List<AudioClip> playlist = new List<AudioClip>();
+
+        private void Start()
+        {
+            OnTrackEnded();
+        }
+
+        private AudioClip GetUnplayedTrack()
+        {
+            if (playlist.Count <= 0)
+                playlist = new List<AudioClip>(tracks);
+            playlist.Shuffle();
+            return playlist.PopRandom();;
+        }
+
+        private void OnAmbianceEnded()
+        {
+            var track = GetUnplayedTrack();
+            musicPlayer.clip = track;
+            musicPlayer.Play();
+            StartCoroutine(StartFade(ambiancePlayer, 5f, 0.2f));
+            StartCoroutine(StartFade(musicPlayer, 2f, 1f));
+            StartCoroutine(Wait(track.length - 2f, OnTrackEnded));
+        }
+        
+        private void OnTrackEnded()
+        {
+            StartCoroutine(StartFade(musicPlayer, 2f, 0f, true));
+            StartCoroutine(StartFade(ambiancePlayer, 5f, 1f));
+            StartCoroutine(Wait(20f, OnAmbianceEnded));
+        }
+
+        private static IEnumerator StartFade(AudioSource audioSource, float duration, float targetVolume, bool stop = false)
+        {
+            var currentTime = 0.0f;
+            var startVolume = audioSource.volume;
+
+            while (currentTime < duration)
+            {
+                currentTime += Time.deltaTime;
+                audioSource.volume = Mathf.Lerp(startVolume, targetVolume, currentTime / duration);
+                yield return null;
+            }
+            if (stop)
+                audioSource.Stop();
+        }
+        
+        private static IEnumerator Wait(float clipLength, Action callback)
+        {
+            yield return new WaitForSeconds(clipLength);
+            callback();
+        }
+        
+        [Button("Skip Section")]
+        private void SkipSection()
+        {
+            StopAllCoroutines();
+            if (musicPlayer.volume >= 1f)
+                OnTrackEnded();
+            else
+                OnAmbianceEnded();
+        }
+    }
+}
