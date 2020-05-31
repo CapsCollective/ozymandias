f7cc95f183bdafe418f452013e5ea88dc8627305
Jordan Hamlin <Jordan.Hamlin6@gmail.com>
2020-05-31 20:03:03 +1000

    New class to handle managing section data.

--- Assets/Scripts/Map/BuildingPlacement/Section.cs
+++ Assets/Scripts/Map/BuildingPlacement/Section.cs
@@ -6,13 +6,12 @@ using UnityEngine;
 public class Section : MonoBehaviour
 {
     // Member variables
-    public int clockwiseRotations;
     public Transform cornerParent;
 
     private MeshFilter _meshFilter;
 
     // Properties
-    public Vector3[] VertexCoordinates { get; private set; }
+    private SectionData sectionData;
 
     // MonoBehaviour Functions
     private void OnDrawGizmos()
@@ -27,35 +26,29 @@ public class Section : MonoBehaviour
     // Class Functions
     public void Fit(Vector3[] corners, float heightFactor)
     {
-        // Rotate the mesh
-        for (int i = 0; i < clockwiseRotations; i++)
-        {
-            Vector3 temp = corners[0];
-            corners[0] = corners[3];
-            corners[3] = corners[2];
-            corners[2] = corners[1];
-            corners[1] = temp;
-        }
-
         _meshFilter = GetComponent<MeshFilter>();
+        Vector3[] worldVertices = _meshFilter.sharedMesh.vertices;
 
-        CalculateVertexCoordinates();
+        for (int i = 0; i < worldVertices.Length; i++)
+            worldVertices[i] = transform.TransformPoint(worldVertices[i]);
 
-        //Debug.Log(corners[0] + " : " + corners[1] + " : " + corners[2] + " : " + corners[3]);
+        sectionData = new SectionData();
+        sectionData.Calculate(worldVertices, cornerParent);
 
         Vector3[] planePositions = new Vector3[_meshFilter.mesh.vertexCount];
 
         for (int i = 0; i < planePositions.Length; i++)
         {
-            Vector3 i0 = Vector3.Lerp(corners[0], corners[3], VertexCoordinates[i].x);
-            Vector3 i1 = Vector3.Lerp(corners[1], corners[2], VertexCoordinates[i].x);
-            Vector3 i2 = Vector3.Lerp(i0, i1, VertexCoordinates[i].z);
+            Vector3 i0 = Vector3.Lerp(corners[0], corners[3], sectionData[i].x);
+            Vector3 i1 = Vector3.Lerp(corners[1], corners[2], sectionData[i].x);
+            Vector3 i2 = Vector3.Lerp(i0, i1, sectionData[i].z);
 
             planePositions[i] = transform.InverseTransformPoint(i2);
-            planePositions[i].y += heightFactor * VertexCoordinates[i].y;
+            planePositions[i].y += heightFactor * sectionData[i].y;
         }
 
         Vector3[] vertices = _meshFilter.mesh.vertices;
+
         for (int i = 0; i < vertices.Length; i++)
         {
             vertices[i] = planePositions[i];
@@ -67,33 +60,50 @@ public class Section : MonoBehaviour
         _meshFilter.mesh.RecalculateTangents();
     }
 
-    public void CalculateVertexCoordinates()
+    [System.Serializable]
+    public class SectionData
     {
-        int vertexCount = _meshFilter.mesh.vertexCount;
-        Vector3[] vertices = _meshFilter.mesh.vertices;
-        VertexCoordinates = new Vector3[vertexCount];
+        private Vector3[] VertexCoordinates;
 
-        Vector3 p0 = cornerParent.GetChild(0).position;
-        Vector3 p1 = cornerParent.GetChild(1).position;
-        Vector3 p2 = cornerParent.GetChild(4).position;
-        Vector3 p3 = cornerParent.GetChild(3).position;
-
-        for (int i = 0; i < vertexCount; i++)
+        public void Calculate(Vector3[] worldVertices, Transform cornerParent)
         {
-            Vector3 p = transform.TransformPoint(vertices[i]);
-            VertexCoordinates[i] = CalculateUV(p, p0, p1, p2, p3);
+            int vertexCount = worldVertices.Length;
+            Vector3[] vertices = worldVertices;
+            VertexCoordinates = new Vector3[vertexCount];
+
+            Vector3 p0 = cornerParent.GetChild(0).position;
+            Vector3 p1 = cornerParent.GetChild(1).position;
+            Vector3 p2 = cornerParent.GetChild(4).position;
+            Vector3 p3 = cornerParent.GetChild(3).position;
+
+            for (int i = 0; i < vertexCount; i++)
+            {
+                Vector3 p = vertices[i];
+                VertexCoordinates[i] = CalculateUV(p, p0, p1, p2, p3);
+            }
         }
-    }
 
-    public Vector3 CalculateUV(Vector3 p, Vector3 a, Vector3 b, Vector3 c, Vector3 d)
-    {
-        return new Vector3(InverseLerp(a, d, p), InverseLerp(a, c, p), InverseLerp(a, b, p));
-    }
+        private static Vector3 CalculateUV(Vector3 p, Vector3 a, Vector3 b, Vector3 c, Vector3 d)
+        {
+            return new Vector3(InverseLerp(a, d, p), InverseLerp(a, c, p), InverseLerp(a, b, p));
+        }
 
-    public float InverseLerp(Vector3 a, Vector3 b, Vector3 p)
-    {
-        Vector3 AB = b - a;
-        Vector3 AP = p - a;
-        return Vector3.Dot(AP, AB) / Vector3.Dot(AB, AB);
+        private static float InverseLerp(Vector3 a, Vector3 b, Vector3 p)
+        {
+            Vector3 AB = b - a;
+            Vector3 AP = p - a;
+            return Vector3.Dot(AP, AB) / Vector3.Dot(AB, AB);
+        }
+
+        public static implicit operator Vector3[](SectionData data)
+        {
+            return data.VertexCoordinates;
+        }
+
+        public Vector3 this[int index]
+        {
+            get => VertexCoordinates[index];
+            set => VertexCoordinates[index] = value;
+        }
     }
 }
