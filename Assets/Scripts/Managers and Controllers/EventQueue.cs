0db60b2355e70c180ce4a94e9e247eabf4318ee7
Swiggies <38583668+Swiggies@users.noreply.github.com>
2020-06-01 21:13:30 +1000

    Update EventQueue.cs

--- Assets/Scripts/Managers and Controllers/EventQueue.cs
+++ Assets/Scripts/Managers and Controllers/EventQueue.cs
@@ -70,7 +70,7 @@ public class EventQueue : MonoBehaviour
         EventType type;
 
         int adventurerW = GameManager.Manager.Satisfaction < 50 ? 13 : 33;
-        if (GameManager.Manager.AvailableAdventurers < GameManager.Manager.Accommodation * 0.5f)
+        if (GameManager.Manager.AvailableAdventurers < GameManager.Manager.Accommodation * 0.75f)
             adventurerW += 13;
         int chaosW = GameManager.Manager.Satisfaction < 50 ? 33 : 13;
         //int flavourW = 99 - chaosW - adventurerW; Don't think I need this
