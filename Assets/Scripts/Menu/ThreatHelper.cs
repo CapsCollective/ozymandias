c4057ac30147019397633d489557c44ac2d14a5a
Ben Kelly <benkellysoftware@gmail.com>
2020-06-02 22:50:34 +1000

    Updated UI Helpers
    Former-commit-id: ee8a8496bdb4c98785328e44e396c6232538b560

--- Assets/Scripts/Menu/ThreatHelper.cs
+++ Assets/Scripts/Menu/ThreatHelper.cs
@@ -2,17 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using static GameManager;
 
 public class ThreatHelper : MonoBehaviour
 {
-    GameManager gameManager;
     void Start()
     {
-        gameManager = FindObjectOfType<GameManager>();
         transform.Find("Text").GetComponent<TextMeshProUGUI>().text =
-            "The threat bar represents how close your town is to impending doom." + "\n" +
-            "Reduce threat by getting more adventurers, improving their efficiency and making your townsfolk more happy" + "\n" +
-            "<b>Currently you have:</b> " + "<i><color=orange>" + gameManager.Threat + "/100 threat" + "</color></i>" + "\n" +
-            "<b>Currently you are gaining:</b> " + "<i><color=orange>" + gameManager.ThreatPerTurn + " threat every turn" + "</color></i>" + "\n";
+            "The threat bar represents how close your town is to impending doom, which will happen if it gets too high." + "\n" +
+            "There's not much you can do to stop monsters from wanting to eat you, but you CAN find and equip adventurers to protect you!" + "\n\n" +
+            "<b>Currently you have:</b> " + "<i><color=orange>" + Manager.Threat + " threat</color></i> against " + "<i><color=#008080>"+ Manager.Defense + " defense</color></i>\n" +
+            "<b>Currently you are gaining:</b> " + "<i><color=orange>" + Manager.ThreatPerTurn + " threat every turn" + "</color></i>" + "\n";
     }
 }
--- Assets/Scripts/Menu/WealthHelper.cs
+++ Assets/Scripts/Menu/WealthHelper.cs
@@ -9,10 +9,9 @@ public class WealthHelper : MonoBehaviour
     void Start()
     {
         gameManager = FindObjectOfType<GameManager>();
-        transform.Find("Text").GetComponent<TextMeshProUGUI>().text = 
-            "Wealth is the currency in which you spend on creating buildings and clearing out terrain" + "\n"
-          + "<b>Currently you have:</b> " + "<i><color=orange>" + gameManager.Wealth + " gold</color></i>" + "\n"
-          + "<b>Currently you are earning:</b> " + "<i><color=orange>" + gameManager.WealthPerTurn + " gold/turn</color></i>";
+        transform.Find("Text").GetComponent<TextMeshProUGUI>().text =
+            "Wealth is the currency in which you spend on performing in game actions, like building, clearing, and questing.\n" +
+            "Wealth gained per turn is based on your number of adventurers multiplied by their spending.";
     }
 
 }
