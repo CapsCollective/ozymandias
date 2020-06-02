cb02671ba73f09cc8761527426aff01ec6fc6340
Jonathan Moallem <jdmoallem@gmail.com>
2020-06-02 12:28:17 +1000

    Made newspaper continue button change text when choices are required
    Former-commit-id: cc7c651018fada93e7258ad497e09fc0f08daa2c

--- Assets/Scripts/Managers and Controllers/NewspaperController.cs
+++ Assets/Scripts/Managers and Controllers/NewspaperController.cs
@@ -6,85 +6,96 @@ using UnityEngine.UI;
 using static GameManager;
 using Random = UnityEngine.Random;
 
-public class NewspaperController : MonoBehaviour
+namespace Managers_and_Controllers
 {
-    // Serialised Fields
-    #pragma warning disable 0649
-    [SerializeField] private Text titleText;
-    [SerializeField] private EventDisplayManager[] articleList;
-    [SerializeField] private Image articleImage;
-    [SerializeField] private Button[] choiceList;
-    [SerializeField] private Button continueButton;
-    [SerializeField] private Button gameOverButton;
-    [SerializeField] private TextMeshProUGUI turnCounter;
-    private Event choiceEvent;
-    private string newspaperTitle;
+    public class NewspaperController : MonoBehaviour
+    {
+        // Serialised Fields
+        [SerializeField] private Text titleText;
+        [SerializeField] private EventDisplayManager[] articleList;
+        [SerializeField] private Image articleImage;
+        [SerializeField] private Button[] choiceList;
+        [SerializeField] private Button continueButton;
+        [SerializeField] private Button gameOverButton;
+        [SerializeField] private TextMeshProUGUI turnCounter;
+        [SerializeField] private GameObject continueButtonContent;
+        [SerializeField] private GameObject disableButtonContent;
+        private Event choiceEvent;
+        private string newspaperTitle;
     
-    private void Awake()
-    {
-        newspaperTitle = GetNewspaperTitle();
-        titleText.text = "{ " + newspaperTitle + " }";
-        EventQueue.OnEventsProcessed += UpdateDisplay;
-    }
+        private void Awake()
+        {
+            newspaperTitle = GetNewspaperTitle();
+            titleText.text = "{ " + newspaperTitle + " }";
+            EventQueue.OnEventsProcessed += UpdateDisplay;
+        }
 
-    public void UpdateDisplay(List<Event> events, List<string> descriptions)
-    {
-        turnCounter.text = newspaperTitle + ", Turn " + Manager.turnCounter;
+        public void UpdateDisplay(List<Event> events, List<string> descriptions)
+        {
+            turnCounter.text = newspaperTitle + ", Turn " + Manager.turnCounter;
         
-        choiceEvent = events[0];
-        if (choiceEvent.choices.Count > 0) continueButton.enabled = false;
+            choiceEvent = events[0];
+            if (choiceEvent.choices.Count > 0) SetContinueButtonEnabled(false);
         
-        // Set the image for the main article and a newspaper title
-        if(events[0].image) articleImage.sprite = events[0].image;
-        articleImage.gameObject.SetActive(events[0].image);
+            // Set the image for the main article and a newspaper title
+            if(events[0].image) articleImage.sprite = events[0].image;
+            articleImage.gameObject.SetActive(events[0].image);
 
-        // Assign the remaining events to the corresponding spots
-        for (int i = 0; i < events.Count; i++)
-            articleList[i].SetEvent(events[i], descriptions[i], i != events.Count - 1);
+            // Assign the remaining events to the corresponding spots
+            for (int i = 0; i < events.Count; i++)
+                articleList[i].SetEvent(events[i], descriptions[i], i != events.Count - 1);
 
-        // Set all event choices on button texts
-        for (var i = 0; i < choiceList.Length; i++) SetChoiceActive(i, i < choiceEvent.choices.Count);
-    }
+            // Set all event choices on button texts
+            for (var i = 0; i < choiceList.Length; i++) SetChoiceActive(i, i < choiceEvent.choices.Count);
+        }
 
-    public void SetChoiceActive(int choice, bool active)
-    {
-        choiceList[choice].gameObject.SetActive(active);
-        if (active) choiceList[choice].GetComponentInChildren<Text>().text = choiceEvent.choices[choice].name;
-    }
+        public void SetChoiceActive(int choice, bool active)
+        {
+            choiceList[choice].gameObject.SetActive(active);
+            if (active) choiceList[choice].GetComponentInChildren<Text>().text = choiceEvent.choices[choice].name;
+        }
     
-    public void OnChoiceSelected(int choice)
-    {
-        articleList[0].AddChoiceOutcome (choiceEvent.MakeChoice(choice));
-        continueButton.enabled = true;
-        for (var i = 0; i < choiceList.Length; i++) SetChoiceActive(i,false);
+        public void OnChoiceSelected(int choice)
+        {
+            articleList[0].AddChoiceOutcome (choiceEvent.MakeChoice(choice));
+            SetContinueButtonEnabled(true);
+            for (var i = 0; i < choiceList.Length; i++) SetChoiceActive(i,false);
         
-        Manager.UpdateUi();
-    }
+            Manager.UpdateUi();
+        }
     
-    private static string[] NewspaperTitles = {
-        "The Wizarding Post", "The Adventurer's Economist", "The Daily Guild", "Dimensional Press",
-        "The Conduit Chronicle", "The Questing Times"
-    };
+        private static string[] NewspaperTitles = {
+            "The Wizarding Post", "The Adventurer's Economist", "The Daily Guild", "Dimensional Press",
+            "The Conduit Chronicle", "The Questing Times"
+        };
 
-    private string GetNewspaperTitle()
-    {
-        Random.InitState((int)DateTime.Now.Ticks);
-        return NewspaperTitles[Random.Range(0, NewspaperTitles.Length)];
-    }
+        private string GetNewspaperTitle()
+        {
+            Random.InitState((int)DateTime.Now.Ticks);
+            return NewspaperTitles[Random.Range(0, NewspaperTitles.Length)];
+        }
 
-    public void GameOver()
-    {
-        continueButton.gameObject.SetActive(false);
-        gameOverButton.gameObject.SetActive(true);
-    }
+        public void GameOver()
+        {
+            continueButton.gameObject.SetActive(false);
+            gameOverButton.gameObject.SetActive(true);
+        }
 
-    public void QuestToMenu()
-    {
-        Manager.menuManager.QuitToMenu();
-    }
-    
-    private void OnDestroy()
-    {
-        EventQueue.OnEventsProcessed -= UpdateDisplay;
+        public void QuestToMenu()
+        {
+            Manager.menuManager.QuitToMenu();
+        }
+
+        private void SetContinueButtonEnabled(bool state)
+        {
+            continueButton.enabled = state;
+            continueButtonContent.SetActive(state);
+            disableButtonContent.SetActive(!state);
+        }
+
+        private void OnDestroy()
+        {
+            EventQueue.OnEventsProcessed -= UpdateDisplay;
+        }
     }
 }
