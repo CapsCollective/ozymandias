5198d7534694a611fc57e148212ad4b82a8d9590
Ben Kelly <benkellysoftware@gmail.com>
2020-06-14 02:47:12 +1000

    Cards pop up and down on turn change, and are randomised on start. [Bug Fix]

--- Assets/Scripts/Managers and Controllers/PlacementController.cs
+++ Assets/Scripts/Managers and Controllers/PlacementController.cs
@@ -31,9 +31,10 @@ public class PlacementController : MonoBehaviour
         ClickManager.OnLeftClick += LeftClick;
         ClickManager.OnRightClick += RightClick;
 
-        OnNewTurn += NewCards;
+        OnNextTurn += NewCards;
         
         remainingBuildings = new List<GameObject>(BuildingManager.BuildManager.AllBuildings);
+        for (int i = 0; i < 3; i++) cards[i].buildingPrefab = remainingBuildings.PopRandom();
     }
 
     void Update()
@@ -82,7 +83,7 @@ public class PlacementController : MonoBehaviour
         int i = Selected;
         if (map.CreateBuilding(cards[i].buildingPrefab, hit.point, rotation))
         {
-            NewCard(i);
+            StartCoroutine(NewCard(i));
             cards[i].toggle.isOn = false;
             Selected = Deselected;
         }
@@ -97,12 +98,25 @@ public class PlacementController : MonoBehaviour
 
     public void NewCards()
     {
-        for (int i = 0; i < 3; i++) NewCard(i);
+        GetComponent<ToggleGroup>().SetAllTogglesOff();
+        for (int i = 0; i < 3; i++) StartCoroutine(NewCard(i));
     }
 
-    public void NewCard(int i)
+    public IEnumerator NewCard(int i)
     {
+        RectTransform transform = cards[i].GetComponent<RectTransform>();
+        for (int j = 0; j < 30; j++)
+        {
+            transform.anchoredPosition -= new Vector2(0,2.5f);
+            yield return null;
+        }
         if (remainingBuildings.Count == 0) remainingBuildings = new List<GameObject>(BuildingManager.BuildManager.AllBuildings);
         cards[i].buildingPrefab = remainingBuildings.PopRandom();
+        Manager.UpdateUi();
+        for (int j = 0; j < 30; j++)
+        {
+            transform.anchoredPosition += new Vector2(0,2.5f);
+            yield return null;
+        }
     }
 }
