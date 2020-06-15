2b4aaa8e8800d3f9a321be98ef886fcf2cdd394a
Edisuism <53289506+Edisuism@users.noreply.github.com>
2020-06-15 20:33:43 +1000

    [US103] initial setup for Ben

---
+++ Assets/Scripts/Menu/Achievement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Achievement : MonoBehaviour
+{
+    public GameObject[] achievements = new GameObject[8];
+    public GameObject progress;
+
+    public void Unlock(int achievementID)
+    {
+        //change achievement color
+        achievements[achievementID].GetComponent<Image>().color = new Color(255, 160, 0, 255);
+        achievements[achievementID].transform.Find("Slider").GetComponent<Slider>().enabled = false;
+
+        //update achievement progress and change tier gem colours accordingly
+        float progressTier = progress.transform.Find("Slider").GetComponent<Slider>().value += 1 / 8f;
+        if (progressTier >= 2 / 8f)
+        {
+            progress.transform.Find("RoadStop").GetComponent<Image>().color = new Color(255, 160, 0, 255);
+        }
+        if (progressTier >= 4 / 8f)
+        {
+            progress.transform.Find("Village").GetComponent<Image>().color = new Color(255, 160, 0, 255);
+        }
+        if (progressTier >= 6 / 8f)
+        {
+            progress.transform.Find("City").GetComponent<Image>().color = new Color(255, 160, 0, 255);
+        }
+        if (progressTier >= 1f)
+        {
+            progress.transform.Find("Kingdom").GetComponent<Image>().color = new Color(255, 160, 0, 255);
+        }
+    }
+}
+    
