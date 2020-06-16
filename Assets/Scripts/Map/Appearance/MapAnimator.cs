f3633d4bea1289c0b4422bc46d1626ad9be27bd3
Jordan Hamlin <Jordan.Hamlin6@gmail.com>
2020-06-16 12:08:02 +1000

    Added Map Animator script to Main scene [US133]

--- Assets/Scripts/Map/Appearance/MapAnimator.cs
+++ Assets/Scripts/Map/Appearance/MapAnimator.cs
@@ -11,17 +11,10 @@ public class MapAnimator : MonoBehaviour
     public string drainTrigger = "Drain";
     public string effectOrigin = "_Origin";
 
-    public AnimationClip drain;
-    public AnimationClip flood;
-    public AnimationClip entry;
-
     public bool flooded = false;
 
     private Animator _animator;
     private MeshRenderer _meshRenderer;
-
-    private int prevSelected = -1;
-    private bool prevClear = false;
 
     private void Awake()
     {
@@ -39,25 +32,6 @@ public class MapAnimator : MonoBehaviour
         if ((PlacementController.Selected == PlacementController.Deselected && !clear.toggle.isOn) && flooded)
             Drain();
 
-        //if (PlacementController.Selected != prevSelected)
-        //{
-        //    Debug.Log(PlacementController.Selected + " : " + prevSelected);
-
-        //    if (PlacementController.Selected != PlacementController.Deselected && _animator.GetCurrentAnimatorClipInfo(0)[0].clip != flood)
-        //        Fill();
-        //    else if(_animator.GetCurrentAnimatorClipInfo(0)[0].clip != drain || _animator.GetCurrentAnimatorClipInfo(0)[0].clip != entry)
-        //        Drain();
-        //}
-
-        //if (clear.toggle.isOn != prevClear)
-        //{
-        //    if (clear.toggle.isOn && _animator.GetCurrentAnimatorClipInfo(0)[0].clip != flood)
-        //        Fill();
-        //    else if (_animator.GetCurrentAnimatorClipInfo(0)[0].clip != drain || _animator.GetCurrentAnimatorClipInfo(0)[0].clip != entry)
-        //        Drain();
-        //}
-        //prevSelected = PlacementController.Selected;
-        //prevClear = clear.toggle.isOn;
     }
 
     public void Drain()
