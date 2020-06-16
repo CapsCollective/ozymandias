8a9d6d5ffb4abad95ddf079f199be2cb82e87e1c
Ben Kelly <benkellysoftware@gmail.com>
2020-06-16 21:50:08 +1000

    Sidebar height scales with modifiers [BugFix]

--- Assets/Scripts/UI/SideBar.cs
+++ Assets/Scripts/UI/SideBar.cs
@@ -7,11 +7,11 @@ using static GameManager;
 
 public class SideBar : UiUpdater
 {
+    public RectTransform parent, mask, layout;
+    
     public TextMeshProUGUI
         adventurers,
         spending,
-        adventurersLarge,
-        spendingLarge,
         spendingModifier,
         effectivenessModifier,
         satisfactionModifier,
@@ -20,8 +20,6 @@ public class SideBar : UiUpdater
     public BarFill
         effectiveness,
         satisfaction,
-        effectivenessLarge,
-        satisfactionLarge,
         equipment,
         weaponry,
         magic,
@@ -29,20 +27,13 @@ public class SideBar : UiUpdater
         entertainment,
         luxury;
 
-    public Toggle toggleSize;
-    public RectTransform toggleSizeIcon;
-    public RectTransform smallPanel;
-    public RectTransform largePanelMask;
-    
     // Update is called once per frame
     public override void UpdateUi()
     {
         bool overCapacity = Manager.AvailableAdventurers - Manager.Accommodation > 0;
-        adventurers.text = (overCapacity ? "<color=red>" : "") + Manager.AvailableAdventurers + (overCapacity ? "</color>" : "") + " / " + Manager.Accommodation;
-        spending.text = "x" + (Manager.Spending / 100f).ToString("0.00");
-        adventurersLarge.text = "Adventurers " + adventurers.text;
-        spendingLarge.text = "Spending " + spending.text;
-
+        adventurers.text = "Adventurers: " + (overCapacity ? "<color=red>" : "") + Manager.AvailableAdventurers + (overCapacity ? "</color>" : "") + " / " + Manager.Accommodation;
+        spending.text = "Spending: x" + (Manager.Spending / 100f).ToString("0.00");
+        
         int mod = Manager.modifiers[Metric.Spending];
         spendingModifier.gameObject.SetActive(mod != 0);
         spendingModifier.text = (mod > 0 ? "<color=green>+" : "<color=red>") + mod + "%</color> from event modifiers"; 
@@ -61,36 +52,15 @@ public class SideBar : UiUpdater
 
         effectiveness.SetBar(Manager.Effectiveness);
         satisfaction.SetBar(Manager.Satisfaction);
-        effectivenessLarge.SetBar(Manager.Effectiveness);
-        satisfactionLarge.SetBar(Manager.Satisfaction);
         equipment.SetBar(Manager.Equipment);
         weaponry.SetBar(Manager.Weaponry);
         magic.SetBar(Manager.Magic);
         food.SetBar(Manager.Food);
         entertainment.SetBar(Manager.Entertainment);
         luxury.SetBar(Manager.Luxury);
-    }
 
-    public void ToggleSize()
-    {
-        /*
-        toggleSizeIcon.Rotate(0,0,180);
-        StartCoroutine(ToggleRoutine(toggleSize.isOn));
-        */
-    }
-
-    public IEnumerator ToggleRoutine(bool dir)
-    {
-        toggleSize.enabled = false;
-        for (int i = 0; i <= 30; i++)
-        {
-            float time = dir ? i / 30f : (30 - i)/30f; // invert for close
-
-            smallPanel.anchoredPosition = new Vector2(Mathf.Lerp(-35, 30, time), smallPanel.localPosition.y);
-            largePanelMask.sizeDelta = new Vector2(Mathf.Lerp(0, 135, time), Mathf.Lerp(160, 300, time));
-            yield return null;
-        }
-        toggleSize.enabled = true;
-        yield return null;
+        LayoutRebuilder.ForceRebuildLayoutImmediate(layout);
+        parent.sizeDelta = new Vector2(145, layout.rect.height);
+        mask.sizeDelta = new Vector2(145, layout.rect.height);
     }
 }
