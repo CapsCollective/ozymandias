c0393ec2cadf50a694c8539f24ae4b5236cd1580
Ben Kelly <benkellysoftware@gmail.com>
2020-06-17 17:53:43 +1000

    Fixed up the UI Helpers. Project is finished so I'm not going to do the US tags anymore JAIME DON'T MARK ME DOWN :)

--- Assets/Scripts/Menu/ThreatHelper.cs
+++ Assets/Scripts/Menu/ThreatHelper.cs
@@ -9,9 +9,9 @@ public class ThreatHelper : MonoBehaviour
     void Start()
     {
         transform.Find("Text").GetComponent<TextMeshProUGUI>().text =
-            "The threat bar represents how close your town is to impending doom, which will happen if it gets too high." + "\n" +
-            "There's not much you can do to stop monsters from wanting to eat you, but you CAN find and equip adventurers to protect you!" + "\n\n" +
-            "<b>Currently you have:</b> " + "<i><color=orange>" + Manager.Threat + " threat</color></i> against " + "<i><color=#008080>"+ Manager.Defense + " defense</color></i>\n";// +
-            //"<b>Currently you are gaining:</b> " + "<i><color=orange>" + Manager.ThreatPerTurn + " threat every turn" + "</color></i>" + "\n";
+            "The threat bar represents the dangers lurking outside your town, which will end your game if it reaches the top." +
+            "There's not much you can do to stop monsters from wanting to eat you, but you CAN find and equip adventurers to protect you!" +
+            "Having a high defense can push the bar back down and keep you safe." + "\n\n" +
+            "Currently you have <color=orange>" + Manager.Threat + " threat</color></i> against <color=#008080>"+ Manager.Defense + " defense</color>";
     }
 }
