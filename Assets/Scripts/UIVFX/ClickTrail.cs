5cbaffde838022f6248a12199f1424686e783f4d
Edisuism <53289506+Edisuism@users.noreply.github.com>
2020-06-24 16:18:17 +1000

    inital effects

---
+++ Assets/Scripts/UIVFX/ClickTrail.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickTrail : MonoBehaviour
+{
+    public GameObject effect;
+    public Transform start;
+    public GameObject end;
+    private Transform[] directions = new Transform[3];
+    private GameObject trail;
+
+    public void StartEffect()
+    {
+        //Instantiate trail object
+        trail = Instantiate(effect, transform.position, Quaternion.identity);
+        trail.transform.SetParent(transform);
+
+        //create directions for the trail object [start, curve, end]
+        directions[0] = start;
+        directions[2] = end.transform;
+        Vector3 curve = new Vector3();
+        curve = start.position + end.transform.position;
+        GameObject placeholder = new GameObject();
+        placeholder.transform.position = new Vector3(curve.x * 1 / 3, curve.y * 2 / 3, 0);
+        directions[1] = placeholder.transform;
+
+        //pass directions to trail object and begin coroutine to vary target shape
+        trail.GetComponent<Trail>().waypointArray = directions;
+        StartCoroutine(Scale(placeholder));
+    }
+
+    IEnumerator Scale(GameObject placeholder)
+    {
+        yield return new WaitForSeconds(0.7f);
+        iTween.ScaleAdd(end, new Vector3(0.1f, 0.1f, 0.1f), 0.1f);
+        yield return new WaitForSeconds(0.1f);
+        iTween.ScaleAdd(end, new Vector3(-0.1f, -0.1f, -0.1f), 0.1f);
+        yield return new WaitForSeconds(0.1f);
+        iTween.ScaleAdd(end, new Vector3(0.1f, 0.1f, 0.1f), 0.1f);
+        yield return new WaitForSeconds(0.05f);
+        iTween.ScaleAdd(end, new Vector3(-0.1f, -0.1f, -0.1f), 0.1f);
+        Destroy(placeholder);
+    }
+}
+
---
+++ Assets/Scripts/UIVFX/Trail.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trail : MonoBehaviour
+{
+    public Transform[] waypointArray = new Transform[3];
+    float percentsPerSecond = 0.4f; 
+    float currentPathPercent = 0.0f;
+
+    private void Start()
+    {
+        /*
+        Vector3 curve = new Vector3();
+        curve = waypointArray[0].position + waypointArray[2].position;
+        GameObject placeholder = new GameObject();
+        placeholder.transform.position = new Vector3(curve.x * 1/3, curve.y * 2/3, 0);
+        waypointArray[1] = placeholder.transform;
+        */
+    }
+
+    void Update()
+    {
+        if (currentPathPercent < 1)
+        {
+            currentPathPercent += percentsPerSecond * Time.deltaTime;
+        iTween.PutOnPath(gameObject, waypointArray, currentPathPercent);
+        percentsPerSecond += 0.018f;
+        }
+        else
+        {
+            StartCoroutine(Decay());
+        }
+    }
+
+    IEnumerator Decay()
+    {
+        yield return new WaitForSeconds(0.2f);
+        Destroy(gameObject);
+    }
+}
