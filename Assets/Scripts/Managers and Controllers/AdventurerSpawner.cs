c0722fbe89c88992e8ffa3ca949ab4200ba5aa39
Jonathan Moallem <jdmoallem@gmail.com>
2020-06-27 12:09:29 +1000

    Simplified adventurer spawner to handling all movement internally

--- Assets/Scripts/Managers and Controllers/AdventurerSpawner.cs
+++ Assets/Scripts/Managers and Controllers/AdventurerSpawner.cs
@@ -1,5 +1,5 @@
+using System.Collections.Generic;
 using System.Linq;
-using Environment;
 using NaughtyAttributes;
 using UnityEngine;
 
@@ -9,11 +9,35 @@ namespace Managers_and_Controllers
     {
         [SerializeField] private GameObject adventurerModel;
         [SerializeField] private Map map;
+        [SerializeField] private float speed = .5f;
+        
         private MapLayout mapLayout;
+        private readonly Dictionary<GameObject, List<Vector3>> activeAdventurers = 
+            new Dictionary<GameObject, List<Vector3>>();
 
         private void Start()
         {
             mapLayout = map.mapLayout;
+        }
+        
+        private void Update()
+        {
+            foreach (var adventurerPath in activeAdventurers)
+            {
+                var adventurer = adventurerPath.Key;
+                if (adventurerPath.Value.Count >= 0)
+                {
+                    var path = adventurerPath.Value;
+                    adventurer.transform.position = Vector3.MoveTowards(
+                        adventurer.transform.position, path[0], speed * Time.deltaTime);
+                    if (Vector3.Distance(adventurer.transform.position, path[0]) < 0.1f)
+                        adventurerPath.Value.RemoveAt(0);
+                }
+                else
+                {
+                    activeAdventurers.Remove(adventurer);
+                }
+            }
         }
 
         [Button("Spawn")]
@@ -21,10 +45,10 @@ namespace Managers_and_Controllers
         {
             var start = mapLayout.RoadGraph.GetData()[0];
             var end = mapLayout.RoadGraph.GetData()[2];
-            var a = CreateAdventurer(start).GetComponent<EnvironmentalAdventurer>();
+            var a = CreateAdventurer(start);
             var path = mapLayout.AStar(mapLayout.RoadGraph,start, end)
                 .Select(vertex => map.transform.TransformPoint(vertex)).ToList();
-            a.SetPath(path);
+            activeAdventurers.Add(a, path);
         }
 
         private GameObject CreateAdventurer(Vertex vertex)
