779ecdc8a6cb438ff116a8d35309e987a8e1cd28
Olly Rybak <ollyrybak@gmail.com>
2020-07-02 13:23:59 +1000

    fixed tooltips. added tweening and simplified the code, hopefully removing any bugs

--- Assets/Scripts/Menu/Tooltip.cs
+++ Assets/Scripts/Menu/Tooltip.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using DG.Tweening;
 /*
  * At the moments works with the place script to handle hover actions (because at the moment, it requires a right click to open up a help
  * This doesn't translate that well when it's scaled to more than just building information (having to right click everything, even knowing to right click)
@@ -19,71 +20,42 @@ public class Tooltip : MonoBehaviour, IPointerExitHandler, IPointerEnterHandler
 
     public float delay = 0.2f;
     public float fadeDuration = 0.3f;
-    
+
     //building[0], threat[1], quest[2], destroy[3], money[4], sidebar[5]
     //public UIType uiType;
     private GameObject tooltipInstance;
+    private CanvasGroup tooltipCanvasGroup;
+    private bool mouseOver;
+    private float mouseTimer = 0f;
+
+    private void Start()
+    {
+        mouseTimer = delay;
+        tooltipInstance = Instantiate(tooltipPrefab, transform, false);
+        tooltipInstance.transform.localPosition = offset;
+        tooltipCanvasGroup = tooltipInstance.GetComponent<CanvasGroup>();
+        tooltipCanvasGroup.alpha = 0;
+        tooltipCanvasGroup.interactable = false;
+    }
+
+    private void Update()
+    {
+        if (mouseTimer >= 0 && mouseOver)
+            mouseTimer -= Time.deltaTime;
+
+        if (mouseTimer < 0)
+            tooltipCanvasGroup.DOFade(1, fadeDuration);
+    }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        StartCoroutine(CreateTooltip());
+        mouseOver = true;
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        Destroy(tooltipInstance);
+        mouseOver = false;
+        mouseTimer = delay;
+        tooltipCanvasGroup.DOFade(0, fadeDuration);
     }
-
-    private IEnumerator CreateTooltip()
-    {
-        tooltipInstance = Instantiate(tooltipPrefab, transform, false);
-        tooltipInstance.transform.localPosition = offset;
-        CanvasGroup canvasGroup = tooltipInstance.GetComponent<CanvasGroup>();
-        canvasGroup.alpha = 0;
-        yield return new WaitForSeconds(delay);
-
-        float counter = 0f;
-        while (counter < fadeDuration)
-        {
-            counter += Time.deltaTime;
-            if (canvasGroup) canvasGroup.alpha = Mathf.Lerp(0, 1, counter / fadeDuration);
-
-            yield return null;
-        }
-    }
-
-    /*private void UIGet(UIType ui)
-    {
-        switch (ui)
-        {
-            case UIType.building:
-                InfoInstantiate(helperPrefab[0], new Vector3(0, 110, 0));
-                if (helper) helper.GetComponent<BuildingHelper>().FillText(gameObject);
-                break;
-            case UIType.threat:
-                InfoInstantiate(helperPrefab[1], new Vector3(0, -80, 0));
-                break;
-            case UIType.quest:
-                InfoInstantiate(helperPrefab[2], new Vector3(0, 60, 0));
-                break;
-            case UIType.destroy:
-                InfoInstantiate(helperPrefab[3], new Vector3(0, 80, 0));
-                break;
-            case UIType.money:
-                InfoInstantiate(helperPrefab[4], new Vector3(50, 110, 0));
-                break;
-            case UIType.adventurers:
-                InfoInstantiate(helperPrefab[5], new Vector3(-100, 0, 0));
-                break;
-            case UIType.satisfaction:
-                InfoInstantiate(helperPrefab[6], new Vector3(-100, 0, 0));
-                break;
-            case UIType.efficiency:
-                InfoInstantiate(helperPrefab[7], new Vector3(-100, 0, 0));
-                break;
-            case UIType.spending:
-                InfoInstantiate(helperPrefab[8], new Vector3(-100, 0, 0));
-                break;
-        }
-    }*/
 }
