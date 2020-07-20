be85b6ed232c52f4b8da9a63defb13bffcefb692
Olly Rybak <ollyrybak@gmail.com>
2020-07-20 17:14:29 +1000

    Updated trees, lowered loading times by making trees not conform to cells and updated blacksmith/herbalist building

--- Assets/Scripts/Environment/PlaceTerrain.cs
+++ Assets/Scripts/Environment/PlaceTerrain.cs
@@ -32,6 +32,7 @@ public class PlaceTerrain : MonoBehaviour
         {
             if (hit.transform.gameObject.layer == LayerMask.NameToLayer("Surface"))
             {
+               
                 map.CreateBuilding(terrainBuilding, hit.point, rotation, animate: false);
                 Destroy(gameObject);
             }
--- Assets/Scripts/Map/BuildingPlacement/BuildingStructure.cs
+++ Assets/Scripts/Map/BuildingPlacement/BuildingStructure.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Managers_and_Controllers;
 using UnityEngine;
 
@@ -8,6 +9,7 @@ public class BuildingStructure : MonoBehaviour
     // Member Variables
     public List<SectionInfo> sections;
     public bool indestructable;
+    public bool fitToCell = false;
 
     public string buildTrigger = "Build";
     public string clearTrigger = "Clear";
@@ -23,16 +25,33 @@ public class BuildingStructure : MonoBehaviour
 
     public void Start()
     {
-        StaticBatchingUtility.Combine(gameObject);
+
     }
 
     // Class Functions
     public void Fit(Vector3[][] vertices, float heightFactor, bool animate = false)
     {
-        for (int i = 0; i < sections.Count; i++)
+        if (fitToCell)
         {
-            Section section = Instantiate(sections[i].prefab, transform).GetComponent<Section>();
-            section.Fit(vertices[i], heightFactor);
+            for (int i = 0; i < sections.Count; i++)
+            {
+                Section section = Instantiate(sections[i].prefab, transform).GetComponent<Section>();
+                section.Fit(vertices[i], heightFactor);
+            }
+        }
+        else
+        {
+            for (int i = 0; i < sections.Count; i++)
+            {
+                Section s = Instantiate(sections[i].prefab, transform).GetComponent<Section>();
+                Vector3 v = new Vector3();
+                v.x = vertices[i].Average(x => x.x);
+                v.z = vertices[i].Average(x => x.z);
+                s.transform.position = v;
+
+                s.transform.eulerAngles = new Vector3(0, Random.value * 360, 0);
+                s.transform.localScale = s.transform.localScale * Random.Range(0.8f, 1.2f);
+            }
         }
 
         if (animate) Animator.SetTrigger(buildTrigger);
