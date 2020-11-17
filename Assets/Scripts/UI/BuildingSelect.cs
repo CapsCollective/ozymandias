1749049bc05da26669069b6e9f7ed05d438d73dc
Jonathan Moallem <jdmoallem@gmail.com>
2020-11-17 14:36:08 +1100

    Made cards play tween animations on hover, and hold position on selection

--- Assets/Scripts/UI/BuildingSelect.cs
+++ Assets/Scripts/UI/BuildingSelect.cs
@@ -4,10 +4,11 @@ using System.Collections.Generic;
 using DG.Tweening;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 using static GameManager;
 
-public class BuildingSelect : UiUpdater
+public class BuildingSelect : UiUpdater, IPointerEnterHandler, IPointerExitHandler
 {
     public const int Deselected = -1;
     public int position;
@@ -19,9 +20,12 @@ public class BuildingSelect : UiUpdater
     public Toggle toggle;
     public CanvasGroup canvasGroup;
     [SerializeField] private Ease tweenEase;
+    [SerializeField] private BuildingSelect[] siblingCards;
 
     private Vector3 _initialPosition;
     private RectTransform _rectTransform;
+    
+    private bool _selected = false;
 
     private void Start()
     {
@@ -52,21 +56,32 @@ public class BuildingSelect : UiUpdater
     public void ToggleSelect()
     {
         PlacementManager.Selected = toggle.isOn ? position : Deselected;
-        SelectTween(toggle.isOn);
     }
 
-    private void SelectTween(bool selected)
+    public void OnPointerEnter(PointerEventData eventData)
     {
-        if (selected)
-        {
-            _rectTransform.DOLocalMove(_initialPosition + _rectTransform.transform.up * 60, 0.5f)
-                .SetEase(tweenEase);
-            _rectTransform.DOScale(new Vector3(1.1f, 1.1f), 0.5f).SetEase(tweenEase);
-        }
-        else
-        {
-            _rectTransform.DOLocalMove(_initialPosition, 0.5f).SetEase(tweenEase);
-            _rectTransform.DOScale(Vector3.one, 0.5f).SetEase(tweenEase);
-        }
+        _rectTransform.DOLocalMove(_initialPosition + _rectTransform.transform.up * 60, 0.5f)
+            .SetEase(tweenEase);
+        _rectTransform.DOScale(new Vector3(1.1f, 1.1f), 0.5f).SetEase(tweenEase);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (_selected) return;
+        _rectTransform.DOLocalMove(_initialPosition, 0.5f).SetEase(tweenEase);
+        _rectTransform.DOScale(Vector3.one, 0.5f).SetEase(tweenEase);
+    }
+
+    public void OnClicked()
+    {
+        _selected = !_selected;
+        OnPointerEnter(null);
+        Array.ForEach(siblingCards, card => card.Deselect());
+    }
+    
+    private void Deselect()
+    {
+        _selected = false;
+        OnPointerExit(null);
     }
 }
