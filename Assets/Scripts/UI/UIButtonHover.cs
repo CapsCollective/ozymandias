5a4cf8ec0232f03288979edbe4a38e137cef1c3e
Jonathan Moallem <jdmoallem@gmail.com>
2021-01-28 13:44:02 +1100

    Added basic on-hover animation for UI buttons

--- Assets/Scripts/UI/UIButtonHover.cs
+++ Assets/Scripts/UI/UIButtonHover.cs
@@ -1,9 +1,31 @@
+using DG.Tweening;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 namespace UI
 {
-    public class UIButtonHover : MonoBehaviour
+    public class UIButtonHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
     {
-        
+        // Private fields
+        private Image _buttonImage;
+
+        private void Start()
+        {
+            // Populate field values
+            _buttonImage = GetComponent<Image>();
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            // Start the on-hover animation
+            _buttonImage.rectTransform.DOScale(1.2f, 0.3f);
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            // End the on-hover animation
+            _buttonImage.rectTransform.DOScale(1.0f, 0.3f);
+        }
     }
 }
