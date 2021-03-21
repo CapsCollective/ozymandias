06a5f840ef120d32c9d7709cbdcef02384e4d99f
Jonathan Moallem <jdmoallem@icloud.com>
2021-03-21 18:28:06 +1100

    Updated UI elements and stability script

--- Assets/Scripts/UI/Stability.cs
+++ Assets/Scripts/UI/Stability.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using static Managers.GameManager;
@@ -8,19 +7,21 @@ namespace UI
 {
     public class Stability : UiUpdater
     {
-        [SerializeField] private RectTransform mask, fill;
-        [SerializeField] private Image chevron;
-        [SerializeField] private List<Sprite> chevronSizes;
-        
-        private const float ChevronThreshold = 10;
+        [SerializeField] private Image bar;
         
         protected override void UpdateUi()
         {
-            mask.DOLocalMoveY((Manager.Stability - 100) * 1.45f, 0.5f);
-            fill.DOLocalMoveY((100 - Manager.Stability) * 1.45f, 0.5f);
-            
-            chevron.color = Manager.ChangePerTurn >= 0 ? new Color(0.37f, 0.73f, 0.19f) : new Color(0.82f, 0.17f, 0.14f);
-            chevron.sprite = chevronSizes[Mathf.Min((int)Mathf.Ceil(Mathf.Abs(Manager.ChangePerTurn / ChevronThreshold)),3)];
+            if (Manager.Stability > 0)
+            {
+                bar.transform.DOScaleX(Manager.Stability/100f, 0.5f);
+            }
+            else
+            {
+                bar.transform.DOScaleX(0, 0.5f).OnComplete(() =>
+                {
+                    bar.color = Color.clear;
+                });
+            }
         }
     }
 }
