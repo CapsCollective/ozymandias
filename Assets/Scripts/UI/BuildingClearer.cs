67f26a172785ea1022d5f94db438617d9bdadb26
Aryeh <aryehzinn@hotmail.com>
2021-03-21 20:23:42 +1100

    Removed restriction for the Guildhouse

--- Assets/Scripts/UI/BuildingClearer.cs
+++ Assets/Scripts/UI/BuildingClearer.cs
@@ -8,32 +8,34 @@ using static Managers.GameManager;
 
 public class BuildingClearer : MonoBehaviour
 {
-    [SerializeField] private Button _clearButton;
+    public Vector2 buttonOffset;
+    
+    private Button _clearButton;
     private Cell _selected;
     private Camera _mainCamera;
 
     void Start()
     {
-        _clearButton = GetComponentInChildren<Button>();
+        _clearButton = GetComponentInChildren<Button>(); 
         
         Click.OnLeftClick += LeftClick;
         Click.OnRightClick += RightClick;
 
-        Clear();
-
         CameraMovement.OnCameraMove += Clear;
 
         _mainCamera = Camera.main;
+        
+        Clear();
     }
 
     private void Update()
     {
-        if (_selected != null && _clearButton.gameObject.activeSelf)
-        {
-            Vector3 buildingPosition = _selected.occupant.transform.position;
-            _clearButton.transform.position = Vector3.Lerp(_clearButton.transform.position,
-                _mainCamera.WorldToScreenPoint(buildingPosition) + (Vector3.up * 70.0f), 0.5f);
-        }
+        if (_selected == null || !_clearButton.gameObject.activeSelf) return;
+
+        MeshRenderer renderer = _selected.occupant.GetComponentInChildren<MeshRenderer>();
+        Vector3 buildingPosition = _selected.occupant.transform.position;
+        _clearButton.transform.position = Vector3.Lerp(_clearButton.transform.position,
+            _mainCamera.WorldToScreenPoint(buildingPosition) + new Vector3(buttonOffset.x, buttonOffset.y, 0.0f), 0.5f);
     }
 
     void Clear()
@@ -45,23 +47,22 @@ public class BuildingClearer : MonoBehaviour
     void LeftClick()
     {
         // If nothing is selected
-        if (BuildingPlacement.Selected == -1 && _selected == null)
-        {
-            _selected = Manager.Map.GetCellFromMouse();
+        if (BuildingPlacement.Selected != -1 || _selected != null) return;
+        
+        _selected = Manager.Map.GetCellFromMouse();
 
-            if (_selected.occupant && _selected.occupant.type != BuildingType.Terrain)
-            {
-                Vector3 buildingPosition = _selected.occupant.transform.position;
-                _clearButton.transform.position = _mainCamera.WorldToScreenPoint(buildingPosition) + (Vector3.up * 70.0f);
-                _clearButton.gameObject.SetActive(true);
-            }
-        }
+        if (!_selected.occupant) return;
+        Vector3 buildingPosition = _selected.occupant.transform.position;
+        _clearButton.transform.position =
+            _mainCamera.WorldToScreenPoint(buildingPosition) + new Vector3(buttonOffset.x, buttonOffset.y, 0.0f);
+        _clearButton.gameObject.SetActive(true);
     }
 
     public void ClearBuilding()
     {
+        // TODO: Work out what the exact mechanics are for removing things (how much does it cost?)
         Building occupant = _selected.occupant;
-        if (_selected.occupant.indestructible || occupant.type == BuildingType.GuildHall) return;
+        if (_selected.occupant.indestructible) return;
         occupant.Clear();
         Manager.Buildings.Remove(occupant);
         Clear();
@@ -71,4 +72,8 @@ public class BuildingClearer : MonoBehaviour
     {
         Clear();
     }
+}
+
+internal class SerlializeFieldAttribute : Attribute
+{
 }
