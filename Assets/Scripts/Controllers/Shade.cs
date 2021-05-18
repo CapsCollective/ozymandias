555b3219a9b2dc161d6033e3af26ab80870c7c6c
Jonathan Moallem <jdmoallem@icloud.com>
2021-05-18 22:39:23 +1000

    Fixed shade bug

--- Assets/Scripts/Controllers/Shade.cs
+++ Assets/Scripts/Controllers/Shade.cs
@@ -24,13 +24,6 @@ namespace Controllers
             Instance = this;
             _image = GetComponent<Image>();
             gameObject.SetActive(false);
-            
-            Managers.GameManager.OnNextTurn += () => {
-                gameObject.SetActive(true);
-            };
-            Managers.GameManager.OnNewTurn += () => {
-                gameObject.SetActive(false);
-            };
         }
 
         public void SetDisplay(bool active, float transitionSpeed = DefaultTransitionSpeed)
