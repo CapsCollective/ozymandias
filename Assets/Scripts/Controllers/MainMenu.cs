54b5c6984eb12efdb13995cb69d2844bdaa2188c
Jonathan Moallem <jdmoallem@icloud.com>
2021-07-22 20:21:05 +1000

    Added UI fades to main menu script

--- Assets/Scripts/Controllers/MainMenu.cs
+++ Assets/Scripts/Controllers/MainMenu.cs
@@ -1,6 +1,5 @@
 using System;
 using Cinemachine;
-using Cinemachine.Utility;
 using UnityEngine;
 
 namespace Controllers
@@ -10,9 +9,15 @@ namespace Controllers
         private const float MenuOrbitHeight = 2.0f;
         private static readonly Vector3 MenuPos = new Vector3(-2.0f, 1.0f, -24.0f);
         
+        [SerializeField] private GameObject gameUI;
         [SerializeField] private GameObject loadingScreenPrefab;
         [SerializeField] private AudioSource menuMusic;
         [SerializeField] private CinemachineFreeLook freeLook;
+        
+        private Canvas _menuCanvas;
+        private Canvas _gameCanvas;
+        private CanvasGroup _menuCanvasGroup;
+        private CanvasGroup _gameCanvasGroup;
 
         enum MenuState
         {
@@ -23,6 +28,28 @@ namespace Controllers
             OpeningMenu
         }
         
+        private readonly struct FadeType
+        {
+            public readonly float Target;
+            public readonly Func<float, bool> Condition;
+
+            public FadeType(float target, Func<float, bool> condition)
+            {
+                Target = target;
+                Condition = condition;
+            }
+        }
+        
+        private static readonly FadeType FadeIn = new FadeType(
+            1.0f,
+            (alpha) => alpha >= 0.99f
+        );
+        
+        private static readonly FadeType FadeOut = new FadeType(
+            0.0f,
+            (alpha) => alpha <= 0.01f
+        );
+        
         private MenuState _menuState = MenuState.Initialising;
         private float _startOrbitHeight;
         private Vector3 _startPos;
@@ -30,6 +57,11 @@ namespace Controllers
 
         private void Start()
         {
+            _menuCanvas = GetComponent<Canvas>();
+            _gameCanvas = gameUI.GetComponent<Canvas>();
+            _menuCanvasGroup = GetComponent<CanvasGroup>();
+            _gameCanvasGroup = gameUI.GetComponent<CanvasGroup>();
+            
             _startPos = freeLook.Follow.position;
             _startOrbitHeight = freeLook.m_Orbits[1].m_Height;
 
@@ -42,32 +74,66 @@ namespace Controllers
             switch (_menuState)
             {
                 case MenuState.Initialising:
+                    _menuCanvas.enabled = true;
+                    _gameCanvas.enabled = false;
                     _menuState = MenuState.InMenu;
                     break;
                 case MenuState.InMenu:
                     break;
                 case MenuState.StartingGame:
-                    StartGame();
+                    StartingGameUpdate();
                     break;
                 case MenuState.Playing:
                     break;
                 case MenuState.OpeningMenu:
+                    _menuCanvas.enabled = true;
+                    _gameCanvas.enabled = false;
                     break;
-                default:
-                    throw new ArgumentOutOfRangeException();
             }
         }
 
-        private void StartGame()
+        private void StartingGameInit()
         {
-            freeLook.Follow.position = Vector3.Lerp(
-                freeLook.Follow.position, _startPos, Time.deltaTime + 0.01f);
-            freeLook.m_Orbits[1].m_Height = Mathf.Lerp(
-                freeLook.m_Orbits[1].m_Height, _startOrbitHeight, Time.deltaTime);
-            if ((_startPos - freeLook.Follow.position).magnitude < UnityVectorExtensions.Epsilon + 1)
+            _menuState = MenuState.StartingGame;
+            _gameCanvasGroup.alpha = 0.0f;
+            _gameCanvas.enabled = true;
+        }
+
+        private void StartingGameUpdate()
+        {
+            var followPos = freeLook.Follow.position;
+            var finishedMoving = (_startPos - followPos).magnitude < 1.0f;
+            if (!finishedMoving)
             {
-                _menuState = MenuState.Playing;
+                // Set follow position
+                followPos = Vector3.Lerp(followPos, _startPos, Time.deltaTime + 0.01f);
+                freeLook.Follow.position = followPos;
+                
+                // Set orbit
+                freeLook.m_Orbits[1].m_Height = Mathf.Lerp(
+                freeLook.m_Orbits[1].m_Height, _startOrbitHeight, Time.deltaTime);
             }
+            
+            var finishedFadingMenu = FadeCanvas(_menuCanvasGroup, FadeOut);
+            if (finishedFadingMenu)
+            {
+                _menuCanvasGroup.alpha = 0.0f;
+                _menuCanvas.enabled = false;
+            }
+            
+            if (!finishedMoving || !finishedFadingMenu) return;
+
+            var finishedFadingGame = FadeCanvas(_gameCanvasGroup, FadeIn);
+            if (!finishedFadingGame) return;
+            
+            _gameCanvasGroup.alpha = 1.0f;
+            _menuState = MenuState.Playing;
+        }
+
+        private static bool FadeCanvas(CanvasGroup canvasGroup, FadeType fadeType)
+        {
+            canvasGroup.alpha = Mathf.Lerp(canvasGroup.alpha, fadeType.Target, Time.deltaTime + 0.1f);
+            return fadeType.Condition(canvasGroup.alpha);
         }
 
         // private void Start()
@@ -87,7 +153,7 @@ namespace Controllers
 
         public void Play()
         {
-            _menuState = MenuState.StartingGame;
+            StartingGameInit();
         }
     }
 }
