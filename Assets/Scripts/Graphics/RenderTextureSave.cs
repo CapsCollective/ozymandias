7d5437becd509364aba86386849bdeb1d5876f40
Olly Rybak <ollyrybak@gmail.com>
2023-01-21 17:31:15 +1100

    Baked shader to texture to optimise performance

--- Assets/Scripts/Graphics/RenderTextureSave.cs
+++ Assets/Scripts/Graphics/RenderTextureSave.cs
@@ -3,20 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using NaughtyAttributes;
 
-[ExecuteAlways]
+[ExecuteInEditMode]
 public class RenderTextureSave : MonoBehaviour
 {
     [SerializeField] private CustomRenderTexture renderTexture;
+    [SerializeField] private string fileName;
 
     [Button]
     public void SaveToTexture()
     {
-        // Currently not used.
-
-        //RenderTexture.active = renderTexture;
-        //Texture2D tex = new Texture2D(renderTexture.width, renderTexture.height);
-        //tex.ReadPixels(new Rect(0, 0, tex.width, tex.height), 0, 0);
-        //byte[] data = tex.EncodeToPNG();
-        //System.IO.File.WriteAllBytes($"{Application.dataPath}/Textures/GrassRT.png", data);
+        RenderTexture.active = renderTexture;
+        Texture2D tex = new Texture2D(renderTexture.width, renderTexture.height);
+        tex.ReadPixels(new Rect(0, 0, tex.width, tex.height), 0, 0);
+        byte[] data = tex.EncodeToPNG();
+        System.IO.File.WriteAllBytes($"{Application.dataPath}/Textures/{fileName}.png", data);
     }
 }
