5d48ac5546e7cc8e82d11aa14878c61e94e8d386
Ben Kelly <benkellysoftware@gmail.com>
2023-01-31 22:41:08 +1100

    Upgrade box cutoff heigh changed since it's taller now

--- Assets/Scripts/Upgrades/Upgrades.cs
+++ Assets/Scripts/Upgrades/Upgrades.cs
@@ -13,6 +13,8 @@ namespace Upgrades
 {
     public class Upgrades: MonoBehaviour
     {
+        const int UpgradeBoxCutoffY = -70;
+
         public static Action<UpgradeType> OnUpgradePurchased;
         public bool BoxOpen { get; private set; }
 
@@ -82,7 +84,7 @@ namespace Upgrades
 
             _selected = upgrade;
             purchaseBox.transform.pivot = new Vector2(0.5f, 1.1f);
-            if(upgrade.transform.localPosition.y < -80) purchaseBox.transform.pivot = new Vector2(0.5f, -0.1f);
+            if (upgrade.transform.localPosition.y < UpgradeBoxCutoffY) purchaseBox.transform.pivot = new Vector2(0.5f, -0.1f);
             purchaseBox.transform.position = upgrade.transform.position;
             purchaseBox.canvas.enabled = true;
             DisplayDetails(upgrade);
