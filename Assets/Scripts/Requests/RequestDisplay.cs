759dfac6a6263e7fb255c01a7f4c818d7c474165
Ben Kelly <benkellysoftware@gmail.com>
2023-05-21 10:52:18 +1000

    Add missing apostrophes, reword request display

--- Assets/Scripts/Requests/RequestDisplay.cs
+++ Assets/Scripts/Requests/RequestDisplay.cs
@@ -71,7 +71,7 @@ namespace Requests
             }
             else if (Request == null)
             {
-                _bookDisplay.description.text = "Nothings here yet!";
+                _bookDisplay.description.text = "Wait to receive a request";
                 _bookDisplay.count.text = "";
                 _bookDisplay.tokens.text = "";
                 _bookDisplay.slider.gameObject.SetActive(false);
